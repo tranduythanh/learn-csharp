99a07dc17b427c3e03684b40b4906ff99a8bcbc2
Trần Duy Thanh <coachtranduythanh@gmail.com>
2022-09-18 10:34:05 +0700

    add more ex

---
+++ ex02/Program.cs
@@ -0,0 +1,57 @@
+﻿class PhanSo
+{
+    int Tu, Mau;
+    // Hàm khởi tạo gán giá trị cố định
+    public PhanSo()
+    {
+        Tu = 0;
+        Mau = 1;
+    }
+    public PhanSo(int x)
+    {
+        Tu = x;
+        Mau = 1;
+    }
+    public PhanSo(int t, int m)
+    {
+        Tu = t;
+        Mau = m;
+    }
+    public void XuatPhanSo()
+    {
+        Console.Write("({0}/{1})", Tu, Mau);
+    }
+    public PhanSo Cong(PhanSo PS2)
+    {
+        int TS = Tu * PS2.Mau + Mau * PS2.Tu;
+        int MS = Mau * PS2.Mau;
+        //Gọi hàm khởi tạo 2 tham số
+        PhanSo KetQua = new PhanSo(TS, MS);
+        return KetQua;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PhanSo p1 = new PhanSo(1);
+        p1.XuatPhanSo();
+        Console.WriteLine();
+
+        PhanSo p2 = new PhanSo(3); // p2 = 3/1
+        p2.XuatPhanSo();
+        Console.WriteLine();
+        Console.WriteLine("Nhap tu so: ");
+        int Ts = int.Parse(Console.ReadLine());
+        Console.WriteLine("Nhap mau so: ");
+        int Ms = int.Parse(Console.ReadLine());
+        PhanSo p3 = new PhanSo(Ts, Ms);
+        p3.XuatPhanSo();
+        Console.WriteLine();
+        p1 = p2.Cong(p3);
+        p1.XuatPhanSo();
+        Console.ReadLine();
+    }
+}
---
+++ ex03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class PassingByVal
+{
+    static void Swap(int a, int b)
+    {
+        int Temp = a;
+        a = b;
+        b = Temp;
+        Console.WriteLine("Trong phuong thuc Swap: a = {0}, b = {1}", a, b);
+    }
+
+    static void Swap2(ref int a, ref int b)
+    {
+        int Temp = a;
+        a = b;
+        b = Temp;
+        Console.WriteLine("Trong phuong thuc Swap: a = {0}, b = {1}", a, b);
+    }
+
+    static void Main(string[] args)
+    {
+        int x = 3, y = 4;
+        Console.WriteLine("Truoc khi goi phuong thuc Swap: x = {0}, y = {1}", x, y);
+        Swap(x, y);
+        Console.WriteLine("Sau khi goi phuong thuc Swap: x= {0},y={1}", x, y);
+
+        Console.WriteLine("Truoc khi goi phuong thuc Swap2: x = {0}, y = {1}", x, y);
+        Swap2(ref x, ref y);
+        Console.WriteLine("Sau khi goi phuong thuc Swap2: x= {0},y={1}", x, y);
+        Console.ReadLine();
+    }
+}
