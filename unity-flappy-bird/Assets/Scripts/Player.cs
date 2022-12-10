7de8c00f08a8f9e5e7b1c86ba9330bdc47165aef
tranduythanh <coachtranduythanh@gmail.com>
2022-12-10 21:18:05 +0700

    now bird can flapp wings

--- unity-flappy-bird/Assets/Scripts/Player.cs
+++ unity-flappy-bird/Assets/Scripts/Player.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    private SpriteRenderer spriteRenderer;
+    // a series image of bird to simulate flying bird
+    public Sprite[] sprites;
+    private int spriteIndex;
     private Vector3 direction;
     public float gravity = -9.8f;
     public float strength = 5f;
     
+    public void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        // Kích hoạt lặp chuỗi hình động tác bay của con chim
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
 
     // Update is called once per frame
     public void Update() {
@@ -18,5 +29,13 @@ public class Player : MonoBehaviour
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite() {
+        spriteIndex++;
+        if (spriteIndex >= sprites.Length) {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
